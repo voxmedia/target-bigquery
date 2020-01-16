b8ef901f3e96986e5c6babfbeb32000af27aac4b
Patrick-Ranjit D. Madsen <pamad05@gmail.com>
2020-01-16 15:47:22 +0100

    move persist_lines_stream into stream.py

--- target_bigquery/__init__.py
+++ target_bigquery/__init__.py
@@ -27,6 +27,7 @@ from google.api_core import exceptions
 from target_bigquery.schema import build_schema, filter
 from target_bigquery.encoders import DecimalEncoder
 from target_bigquery.job import persist_lines_job
+from target_bigquery.stream import persist_lines_stream
 from target_bigquery.utils import emit_state
 
 try:
@@ -54,98 +55,6 @@ StreamMeta = collections.namedtuple(
 
 def clear_dict_hook(items):
     return {k: v if v is not None else "" for k, v in items}
-
-
-def persist_lines_stream(project_id, dataset_id, lines=None, validate_records=True):
-    state = None
-    schemas = {}
-    key_properties = {}
-    tables = {}
-    rows = {}
-    errors = {}
-
-    bigquery_client = bigquery.Client(project=project_id)
-
-    dataset_ref = bigquery_client.dataset(dataset_id)
-    dataset = Dataset(dataset_ref)
-    try:
-        dataset = bigquery_client.create_dataset(Dataset(dataset_ref)) or Dataset(
-            dataset_ref
-        )
-    except exceptions.Conflict:
-        pass
-
-    for line in lines:
-        try:
-            msg = singer.parse_message(line)
-        except json.decoder.JSONDecodeError:
-            logger.error("Unable to parse:\n{}".format(line))
-            raise
-
-        if isinstance(msg, singer.RecordMessage):
-            if msg.stream not in schemas:
-                raise Exception(
-                    "A record for stream {} was encountered before a corresponding schema".format(
-                        msg.stream
-                    )
-                )
-
-            schema = schemas[msg.stream]
-
-            if validate_records:
-                validate(msg.record, schema)
-
-            err = None
-            try:
-                err = bigquery_client.insert_rows_json(tables[msg.stream], [msg.record])
-            except Exception as exc:
-                logger.error(
-                    f"failed to insert rows for {tables[msg.stream]}: {str(exc)}\n{msg.record}"
-                )
-                raise
-
-            errors[msg.stream] = err
-            rows[msg.stream] += 1
-
-            state = None
-
-        elif isinstance(msg, singer.StateMessage):
-            logger.debug("Setting state to {}".format(msg.value))
-            state = msg.value
-
-        elif isinstance(msg, singer.SchemaMessage):
-            table = msg.stream
-            schemas[table] = msg.schema
-            key_properties[table] = msg.key_properties
-            tables[table] = bigquery.Table(
-                dataset.table(table), schema=build_schema(schemas[table])
-            )
-            rows[table] = 0
-            errors[table] = None
-            try:
-                tables[table] = bigquery_client.create_table(tables[table])
-            except exceptions.Conflict:
-                pass
-
-        elif isinstance(msg, singer.ActivateVersionMessage):
-            # This is experimental and won't be used yet
-            pass
-
-        else:
-            raise Exception("Unrecognized message {}".format(msg))
-
-    for table in errors.keys():
-        if not errors[table]:
-            logging.info(
-                "Loaded {} row(s) from {} into {}:{}".format(
-                    rows[table], dataset_id, table, tables[table].path
-                )
-            )
-            emit_state(state)
-        else:
-            logging.error("Errors: %s", errors[table])
-
-    return state
 
 
 def collect():
