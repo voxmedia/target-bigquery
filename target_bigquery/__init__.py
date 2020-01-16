26c6f5bd97a5f8410ad2a6a82e0f05f9ecd8b968
Patrick-Ranjit D. Madsen <pamad05@gmail.com>
2020-01-16 15:25:47 +0100

    move persist_lines_job into separate file

--- target_bigquery/__init__.py
+++ target_bigquery/__init__.py
@@ -26,6 +26,7 @@ from google.api_core import exceptions
 
 from target_bigquery.schema import build_schema, filter
 from target_bigquery.encoders import DecimalEncoder
+from target_bigquery.job import persist_lines_job
 
 try:
     parser = argparse.ArgumentParser(parents=[tools.argparser])
@@ -60,109 +61,6 @@ def emit_state(state):
 
 def clear_dict_hook(items):
     return {k: v if v is not None else "" for k, v in items}
-
-
-def persist_lines_job(
-    project_id,
-    dataset_id,
-    lines=None,
-    truncate=False,
-    validate_records=True,
-    table_suffix=None,
-):
-    state = None
-    schemas = {}
-    key_properties = {}
-    rows = {}
-    errors = {}
-    table_suffix = table_suffix or ""
-
-    bigquery_client = bigquery.Client(project=project_id)
-
-    for line in lines:
-        try:
-            msg = singer.parse_message(line)
-        except json.decoder.JSONDecodeError:
-            logger.error("Unable to parse:\n{}".format(line))
-            raise
-
-        if isinstance(msg, singer.RecordMessage):
-            table_name = msg.stream + table_suffix
-
-            if table_name not in schemas:
-                raise Exception(
-                    "A record for stream {} was encountered before a corresponding schema".format(
-                        table_name
-                    )
-                )
-
-            schema = schemas[table_name]
-
-            if validate_records:
-                validate(msg.record, schema)
-
-            new_rec = filter(schema, msg.record)
-
-            # NEWLINE_DELIMITED_JSON expects literal JSON formatted data, with a newline character splitting each row.
-            data = bytes(json.dumps(new_rec, cls=DecimalEncoder) + "\n", "UTF-8")
-
-            rows[table_name].write(data)
-
-            state = None
-
-        elif isinstance(msg, singer.StateMessage):
-            logger.debug("Setting state to {}".format(msg.value))
-            state = msg.value
-
-        elif isinstance(msg, singer.SchemaMessage):
-            table_name = msg.stream + table_suffix
-
-            if table_name in rows:
-                continue
-
-            schemas[table_name] = msg.schema
-            key_properties[table_name] = msg.key_properties
-            rows[table_name] = TemporaryFile(mode="w+b")
-            errors[table_name] = None
-
-        elif isinstance(msg, singer.ActivateVersionMessage):
-            # This is experimental and won't be used yet
-            pass
-
-        else:
-            raise Exception("Unrecognized message {}".format(msg))
-
-    for table in rows.keys():
-        table_ref = bigquery_client.dataset(dataset_id).table(table)
-        SCHEMA = build_schema(schemas[table])
-        load_config = LoadJobConfig()
-        load_config.schema = SCHEMA
-        load_config.source_format = SourceFormat.NEWLINE_DELIMITED_JSON
-
-        if truncate:
-            load_config.write_disposition = WriteDisposition.WRITE_TRUNCATE
-
-        logger.info("loading {} to Bigquery.\n".format(table))
-
-        try:
-            load_job = bigquery_client.load_table_from_file(
-                rows[table], table_ref, job_config=load_config, rewind=True
-            )
-            logger.info("loading job {}".format(load_job.job_id))
-            logger.info(load_job.result())
-        except exceptions.BadRequest as err:
-            logger.error(
-                "failed to load table {} from file: {}".format(table, str(err))
-            )
-            if load_job.errors:
-                messages = [
-                    f"reason: {err['reason']}, message: {err['message']}"
-                    for err in load_job.errors
-                ]
-                logger.error("errors:\n{}".format("\n".join(messages)))
-            raise
-
-    return state
 
 
 def persist_lines_stream(project_id, dataset_id, lines=None, validate_records=True):
