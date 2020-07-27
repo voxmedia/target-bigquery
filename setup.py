722f7e8c4465539d63abc70bf712ebe38fe50523
Anže Kravanja <anze@adswerve.com>
2020-07-27 11:43:35 -0700

    version set to 0.9.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="target-bigquery",
-    version="1.5.0",
+    version="0.9.1",
     description="Singer.io target for writing data to Google BigQuery",
     author="RealSelf Business Intelligence", # is it legal to change this to AdSwerve
     url="https://github.com/adswerve/target-bigquery",
@@ -14,7 +14,6 @@ setup(
         "singer-python==5.9.0",
         "google-cloud==0.34.0",
         "google-cloud-bigquery==1.25.0",
-        # "google-auth==1.19.2",
     ],
     entry_points="""
         [console_scripts]
