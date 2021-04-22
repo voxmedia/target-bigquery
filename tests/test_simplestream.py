22dd4932da020d7732b4f48fdc448bcc280531a6
RuslanBergenov <bergenov@ualberta.ca>
2021-04-21 18:33:21 -0600

    docs: job load unit test comments. fix: minor bug in job load unit test

--- tests/test_simplestream.py
+++ tests/test_simplestream.py
@@ -1,5 +1,20 @@
 from tests import unittestcore
 
+"""
+Configuring data load tests.
+
+    Inside your sandbox sub-directory (under t-bq project root dir), create a target-config file
+    
+    Example
+    {
+        "project_id": "{your_project_id}",
+        "dataset_id": "{your_dataset_id}"
+    }
+
+    Manually create a dataset in BQ which has the same name as in your target-config file.
+
+    TODO: find out why these tests do not create BQ datasets.
+"""
 
 class TestSimpleStreamLoadJob(unittestcore.BaseUnitTest):
 
@@ -41,7 +56,7 @@ class TestSimpleStreamLoadJob(unittestcore.BaseUnitTest):
         self.assertEqual(ret, 0, msg="Exit code is not 0!")
         self.assertDictEqual(state, {"bookmarks": {"simple_stream": {"timestamp": "2020-01-11T00:00:00.000000Z"}}})
 
-        table = self.client.get_table("{}.simple_stream_dev".format(self.dataset_id))
+        table = self.client.get_table("{}.simple_stream".format(self.dataset_id))
         self.assertEqual(3, table.num_rows, msg="Number of rows mismatch")
         self.assertIsNotNone(table.clustering_fields)
         self.assertIsNotNone(table.partitioning_type)
@@ -131,6 +146,51 @@ class TestSimpleStreamPartialLoadJob(unittestcore.BaseUnitTest):
         self.assertIsNone(table.partitioning_type)
 
     def test_simple_stream_with_tables_config(self):
+        from target_bigquery import main
+
+        self.set_cli_args(
+            stdin="./rsc/simple_stream.json",
+            config="../sandbox/target_config.json",
+            tables="./rsc/simple_stream_table_config.json",
+            processhandler="partial-load-job"
+        )
+
+        ret = main()
+        state = self.get_state()[-1]
+        print(state)
+
+        self.assertEqual(ret, 0, msg="Exit code is not 0!")
+        self.assertDictEqual(state, {"bookmarks": {"simple_stream": {"timestamp": "2020-01-11T00:00:00.000000Z"}}})
+
+        table = self.client.get_table("{}.simple_stream".format(self.dataset_id))
+        self.assertEqual(3, table.num_rows, msg="Number of rows mismatch")
+        self.assertIsNotNone(table.clustering_fields)
+        self.assertIsNotNone(table.partitioning_type)
+
+
+    def test_simple_stream_with_tables_config_passed_inside_target_config_file(self):
+
+        """
+        The purpose of this unit test is to test a feature discussed here:
+        https://github.com/adswerve/target-bigquery/issues/15
+
+        Feature:
+        Passing target tables config file (contains partitioning and clustering info) inside target config file.
+
+
+        Configuring test.
+
+            Inside your sandbox directory, create a target-config file
+
+            {
+                "project_id": "{your_project_id}",
+                "dataset_id": "{your_dataset_id}"
+            }
+
+            Manually create a dataset in BQ which has the same name as in your target-config file.
+
+            TODO: find out why these tests do not create BQ datasets.
+        """
         from target_bigquery import main
 
         self.set_cli_args(
