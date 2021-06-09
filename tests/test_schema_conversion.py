849ec2c0ffc755d4460d4492ecfafe41e57a60dc
RuslanBergenov <bergenov@ualberta.ca>
2021-06-09 16:23:13 -0600

    refactor: reduced repetition in schema conversion unit test

--- tests/test_schema_conversion.py
+++ tests/test_schema_conversion.py
@@ -264,40 +264,12 @@ class TestSchemaConversion(unittestcore.BaseUnitTest):
 
     def test_several_nested_schemas_asana(self):
 
-        catalog = json.load(open("./rsc/input_json_schemas_asana.json"))
+        """
+        for some reason recharge product stream schema conversion fails on the old schema.py
+        works with the new schema conversion
+        """
 
-        for next_schema_input in catalog['streams']:
-
-            if next_schema_input['tap_stream_id'] == 'workspaces': # old conversion fails here
-                continue
-
-            validate_json_schema_completeness(next_schema_input)
-
-            schema_0_input = copy.deepcopy(next_schema_input)
-
-            schema_0_input.update({"type": "SCHEMA"})
-
-            schema_0_input = str(schema_0_input)
-
-            schema_0_input = schema_0_input.replace("\'", "\"").replace("True","true").replace("False","false").replace("None","null")
-
-            msg = singer.parse_message(schema_0_input)
-
-            schema_1_simplified = simplify(msg.schema)
-
-            schema_2_built_new_method = build_schema(schema_1_simplified, key_properties=msg.key_properties,
-                                                         add_metadata=True)
-
-            schema_3_built_old_method = build_schema_old(msg.schema, key_properties=msg.key_properties, add_metadata=True)
-
-            # are results of the two methods above identical? ignore order of columns and case
-            schema_built_new_method_sorted = convert_list_of_schema_fields_to_list_of_lists(schema_2_built_new_method)
-
-            schema_built_old_method_sorted = convert_list_of_schema_fields_to_list_of_lists(schema_3_built_old_method)
-
-            assert schema_built_new_method_sorted == schema_built_old_method_sorted
-
-            # TODO: check data types
+        compare_old_vs_new_schema_conversion("./rsc/input_json_schemas_asana.json", exclude_stream='workspaces')
 
 
     def test_several_nested_schemas_asana_workspaces_new_method(self):
@@ -374,6 +346,42 @@ class TestSchemaConversion(unittestcore.BaseUnitTest):
         """
 
         compare_old_vs_new_schema_conversion("./rsc/input_json_schemas_recharge.json", exclude_stream='products')
+
+
+    def test_several_nested_schemas_recharge_products_new_method(self):
+
+        catalog = json.load(open("./rsc/input_json_schemas_recharge.json"))
+
+        for next_schema_input in catalog['streams']:
+
+            if next_schema_input['tap_stream_id'] == 'products': # old conversion fails here
+
+                validate_json_schema_completeness(next_schema_input)
+
+                schema_0_input = copy.deepcopy(next_schema_input)
+
+                schema_0_input.update({"type": "SCHEMA"})
+
+                schema_0_input = str(schema_0_input)
+
+                schema_0_input = schema_0_input.replace("\'", "\"").replace("True","true").replace("False","false").replace("None","null")
+
+                msg = singer.parse_message(schema_0_input)
+
+                schema_1_simplified = simplify(msg.schema)
+
+                schema_2_built_new_method = build_schema(schema_1_simplified, key_properties=msg.key_properties,
+                                                             add_metadata=True)
+
+                # old conversion fails on asana workspaces
+                # schema_3_built_old_method = build_schema_old(msg.schema, key_properties=msg.key_properties, add_metadata=True)
+
+                # are results of the two methods above identical? ignore order of columns and case
+                schema_built_new_method_sorted = convert_list_of_schema_fields_to_list_of_lists(schema_2_built_new_method)
+
+                assert schema_built_new_method_sorted
+
+            # TODO: check data types
 
 
     def test_several_nested_schemas_salesforce(self):
