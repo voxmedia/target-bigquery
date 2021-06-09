2537db319ddd79390874fa8d82bd9cb034113c83
RuslanBergenov <bergenov@ualberta.ca>
2021-06-09 12:27:19 -0600

    refactor: schema conversion test - asana

--- tests/test_schema.py
+++ tests/test_schema.py
@@ -24,8 +24,6 @@ from tests.rsc.input_json_schemas import *
 
 from tests.rsc.input_json_schemas_recharge import *
 
-from tests.rsc.input_json_schemas_asana import *
-
 from tests.rsc.input_json_schemas_bing_ads import *
 
 from tests.rsc.input_json_schemas_klaviyo import *
@@ -305,44 +303,25 @@ class TestStream(unittestcore.BaseUnitTest):
 
             # TODO: check data types
 
-    def test_several_nested_schemas_asana_only_new_method(self):
-
-        list_of_schema_inputs_asana = [ asana_users,
-                                        asana_projects,
-                                        asana_workspaces, # old schema conversion fails here
-                                        asana_stories,
-                                        asana_sections,
-                                        asana_teams,
-                                        asana_tasks,
-                                        asana_tags]
-
-        for next_schema_input in list_of_schema_inputs_asana:
-
-            schema_0_input = next_schema_input
-
-            msg = singer.parse_message(schema_0_input)
-
-            schema_1_simplified = simplify(msg.schema)
-
-            schema_2_built_new_method = build_schema(schema_1_simplified, key_properties=msg.key_properties,
-                                                         add_metadata=True)
-
-            assert schema_2_built_new_method
 
     def test_several_nested_schemas_asana(self):
 
-        list_of_schema_inputs_asana = [ asana_users,
-                                        asana_projects,
-                                        # asana_workspaces, # old schema conversion fails here
-                                        asana_stories,
-                                        asana_sections,
-                                        asana_teams,
-                                        asana_tasks,
-                                        asana_tags]
+        catalog = json.load(open("./rsc/input_json_schemas_asana.json"))
 
-        for next_schema_input in list_of_schema_inputs_asana:
+        for next_schema_input in catalog['streams']:
 
-            schema_0_input = next_schema_input
+            if next_schema_input['tap_stream_id'] == 'workspaces': # old conversion fails here
+                continue
+
+            validate_json_schema_completeness(next_schema_input)
+
+            schema_0_input = copy.deepcopy(next_schema_input)
+
+            schema_0_input.update({"type": "SCHEMA"})
+
+            schema_0_input = str(schema_0_input)
+
+            schema_0_input = schema_0_input.replace("\'", "\"").replace("True","true").replace("False","false").replace("None","null")
 
             msg = singer.parse_message(schema_0_input)
 
@@ -361,6 +340,43 @@ class TestStream(unittestcore.BaseUnitTest):
             assert schema_built_new_method_sorted == schema_built_old_method_sorted
 
             # TODO: check data types
+
+
+    def test_several_nested_schemas_asana_workspaces_new_method(self):
+
+        catalog = json.load(open("./rsc/input_json_schemas_asana.json"))
+
+        for next_schema_input in catalog['streams']:
+
+            if next_schema_input['tap_stream_id'] == 'workspaces': # old conversion fails here
+
+                validate_json_schema_completeness(next_schema_input)
+
+                schema_0_input = copy.deepcopy(next_schema_input)
+
+                schema_0_input.update({"type": "SCHEMA"})
+
+                schema_0_input = str(schema_0_input)
+
+                schema_0_input = schema_0_input.replace("\'", "\"").replace("True","true").replace("False","false").replace("None","null")
+
+                msg = singer.parse_message(schema_0_input)
+
+                schema_1_simplified = simplify(msg.schema)
+
+                schema_2_built_new_method = build_schema(schema_1_simplified, key_properties=msg.key_properties,
+                                                             add_metadata=True)
+
+                # old conversion fails on asana workspaces
+                # schema_3_built_old_method = build_schema_old(msg.schema, key_properties=msg.key_properties, add_metadata=True)
+
+                # are results of the two methods above identical? ignore order of columns and case
+                schema_built_new_method_sorted = convert_list_of_schema_fields_to_list_of_lists(schema_2_built_new_method)
+
+                assert schema_built_new_method_sorted
+
+            # TODO: check data types
+
 
     def test_several_nested_schemas_bing_ads(self):
 
