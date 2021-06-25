f2e7d6abcce44d21a01f2abff8b0e817b9268b6c
RuslanBergenov <bergenov@ualberta.ca>
2021-06-25 16:01:49 -0600

    docs: comments in init.py

--- target_bigquery/__init__.py
+++ target_bigquery/__init__.py
@@ -26,26 +26,33 @@ def main():
                         choices=["load-job", "partial-load-job", "bookmarks-partial-load-job"],
                         default="partial-load-job"
     )
+
+    # parse command line arguments (e.g., target config file path, state, table config file path, process handler type)
     flags = parser.parse_args()
 
+    # read target-config file into a dict
     with open(flags.config) as f:
         config = json.load(f)
 
+    # target tables config (e.g, partitioning and clustering)
     table_config = flags.tables or config.get("table_config")
     tables = {}
     if table_config:
         with open(table_config) as f:
             tables = json.load(f)
 
+    # state
     state = None
     if flags.state is not None:
         with open(flags.state) as f:
             state = json.load(f)
 
+    # determine replication method: append vs. truncate
     truncate = False
     if config.get("replication_method", "append").lower() == "truncate":
         truncate = True
 
+    # arguments supplied in target config
     table_prefix = config.get("table_prefix", "")
     table_suffix = config.get("table_suffix", "")
     location = config.get("location", "US")
@@ -54,7 +61,7 @@ def main():
     project_id, dataset_id = config["project_id"], config["dataset_id"]
 
     table_configs = tables.get("streams", {})
-    max_cache = 1024 * 1024 * config.get("max_cache", 50)
+    max_cache = 1024 * 1024 * config.get("max_cache", 50) # this is needed for partial loads
 
     tap_stream = io.TextIOWrapper(sys.stdin.buffer, encoding="utf-8")
 
@@ -64,7 +71,9 @@ def main():
         from target_bigquery.processhandler import LoadJobProcessHandler, PartialLoadJobProcessHandler, \
             BookmarksStatePartialLoadJobProcessHandler
 
+        # determine process handler
         ph = None
+
         if flags.processhandler == "load-job":
             ph = LoadJobProcessHandler
         elif flags.processhandler == "partial-load-job":
@@ -90,6 +99,7 @@ def main():
             max_cache=max_cache
         )
 
+        # write a state file
         for state in state_iterator:
             emit_state(state)
 
