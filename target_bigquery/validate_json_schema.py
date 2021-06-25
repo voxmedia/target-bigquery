a67e6b1a2fca712f292b1cd32347b16cb8e260ee
RuslanBergenov <bergenov@ualberta.ca>
2021-06-25 14:15:40 -0600

    refactor: reformat validate_json_schema.py

--- target_bigquery/validate_json_schema.py
+++ target_bigquery/validate_json_schema.py
@@ -3,32 +3,34 @@ import singer
 
 LOGGER = singer.get_logger()
 
+
 def validate_json_schema_completeness(schema_input):
     """
     Background
         - We already do have schema validation implemented with json schema library.
-        - However, we had situations when a schema had empty properties, but existing json schema validator didn't flag it as invalid/incomplete (it happened with Shopify schema).
+        - However, we had situations when a schema had empty properties, but existing json schema validator didn't flag
+            it as invalid/incomplete (it happened with Shopify schema).
 
     The purpose of this function is
         - to flag schema as invalid if it is incomplete.
 
-    :param schema_input_as_string: JSON schema. It can be dictionary/JSON object or string.
+    :param schema_input: JSON schema. It can be dictionary/JSON object or string.
 
     What the function does
 
-    Clean input:
-        - If it's not a string, we'll convert it to string.
+        Clean input:
+            - If it's not a string, we'll convert it to string.
 
-    Validation steps:
-        - If schema has empty {} inside its "properties", "type" or "items", fail schema completeness validation check.
-            - To help the end user, we'll tell them which part of schema has empty {} (properties, type or items).
-        - If schema has empty {} elsewhere, a warning is logged.
+        Validation steps:
+            - If schema has empty {} inside its "properties", "type" or "items", fail schema completeness validation check.
+                - To help the end user, we'll tell them which part of schema has empty {} (properties, type or items).
+            - If schema has empty {} elsewhere, a warning is logged.
 
-    Rationale for our validation steps:
-        - empty props, type or items clearly indicate invalid/incomplete schema
-        - {} elsewhere also indicates invalid/incomplete schema, it may cause pipeline failure,
-            but we want to relax the rules a bit
-            and we don't want to be too heavy-handed
+        Rationale for our validation steps:
+            - empty props, type or items clearly indicate invalid/incomplete schema
+            - {} elsewhere also indicates invalid/incomplete schema, it may cause pipeline failure,
+                but we want to relax the rules a bit
+                and we don't want to be too heavy-handed
     """
 
     # convert to string
@@ -41,14 +43,13 @@ def validate_json_schema_completeness(schema_input):
     # the purpose of the dictionary below is to create a more user-friendly error message and point the end user to the
     # correponding part of their schema (props, type, items or elsewhere)
     completeness_validation_dict_exception = {"properties": re.compile(r'\"properties\"\:\{\}'),
-                                    "type": re.compile(r'\"type\"\:\[\]'),
-                                    "items": re.compile(r'\"items\"\:\{\}')
-                                    }
-
-    completeness_validation_dict_warning = {
-                                              "object/dictionary": re.compile(r'\{\}')
+                                              "type": re.compile(r'\"type\"\:\[\]'),
+                                              "items": re.compile(r'\"items\"\:\{\}')
                                               }
 
+    completeness_validation_dict_warning = {
+        "object/dictionary": re.compile(r'\{\}')
+    }
 
     # raise exception
     for schema_element, pattern_not_valid in completeness_validation_dict_exception.items():
@@ -60,5 +61,4 @@ def validate_json_schema_completeness(schema_input):
     for schema_element, pattern_not_valid in completeness_validation_dict_warning.items():
 
         if pattern_not_valid.search(schema_input_no_spaces):
-
             LOGGER.warning("the pipeline might fail because of undefined fields: {}")
