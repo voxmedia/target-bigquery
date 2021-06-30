250a0c605b6d980d21f14398d5e897084b901627
RuslanBergenov <bergenov@ualberta.ca>
2021-06-29 19:41:04 -0600

    refactor: requirements

--- setup.py
+++ setup.py
@@ -2,19 +2,23 @@
 
 from setuptools import setup
 
+install_requires = open("requirements.txt").read().strip().split("\n")
+dev_requires = open("dev-requirements.txt").read().strip().split("\n")
+
+extras = {
+    "dev": dev_requires
+}
+
 setup(
     name="target-bigquery",
-    version="0.10.2",
+    version="0.11.0",
     description="Singer.io target for writing data to Google BigQuery",
     author="Adswerve",
     url="https://github.com/adswerve/target-bigquery",
     classifiers=["Programming Language :: Python :: 3 :: Only"],
     packages=["target_bigquery"],
-    install_requires=[
-        "singer-python==5.9.0",
-        "google-cloud==0.34.0",
-        "google-cloud-bigquery==1.25.0",
-    ],
+    install_requires=install_requires,
+    extras_require=extras,
     entry_points="""
         [console_scripts]
         target-bigquery=target_bigquery:main
