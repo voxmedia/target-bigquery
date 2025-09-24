eb6643783955b2c4f0b3b2f895bfbc8897939029
Hunter Kuffel <hkuffel1217@gmail.com>
2025-09-24 15:29:12 -0600

    log updates

--- target_bigquery/process.py
+++ target_bigquery/process.py
@@ -1,6 +1,7 @@
 import json
 
 import singer
+import simplejson
 
 from target_bigquery.processhandler import BaseProcessHandler
 
@@ -47,6 +48,9 @@ def process(
             logger.info(f"Parsing message: {line}")
             msg = singer.parse_message(line)
         except json.decoder.JSONDecodeError:
+            logger.error("Unable to parse:\n{}".format(line))
+            raise
+        except simplejson.errors.JSONDecodeError:
             logger.error("Unable to parse:\n{}".format(line))
             raise
 
